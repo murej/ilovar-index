0cb213722ddf50d0d7e9462d42c2b0c2cd2857c9
murej <jure.martinec@gmail.com>
2017-04-23 04:44:32 +0200

    Convert gsx internally

--- src/google-spreadsheet-store.js
+++ src/google-spreadsheet-store.js
@@ -20,8 +20,22 @@ export default class GoogleSpreadsheetStore {
   }
 
   getSpreadsheet(apiId) {
-    const query = `http://gsx2json.com/api?id=${apiId}`;
+    // const query = `http://gsx2json.com/api?id=${apiId}`;
+    const query = `https://spreadsheets.google.com/feeds/list/${apiId}/od6/public/values?alt=json`;
 
-    return this.fetchContent(query).then(sheet => sheet);
+    return this.fetchContent(query).then(data => this.prettifyGoogleSheetsJSON(data));
+  }
+
+  prettifyGoogleSheetsJSON(data) {
+    for (var i = 0; i < data.feed.entry.length; i++) {
+        for (var key in data.feed.entry[i]) {
+            if (data.feed.entry[i].hasOwnProperty(key) && key.substr(0,4) === 'gsx$') {
+                // copy the value in the key up a level and delete the original key
+                data.feed.entry[i][key.substr(4)] = data.feed.entry[i][key].$t;
+                delete data.feed.entry[i][key];
+            }
+        }
+    }
+    return data.feed.entry;
   }
 }
